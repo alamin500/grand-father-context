ea833f2572f0713510b80321c7bff94246887561
alamin500 <nalamin500@gmail.com>
2021-10-02 12:54:13 +0600

    h

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import GrandFather from './components/GrandFather/GrandFather';
-import { useState } from 'react';
+import { createContext, useState } from 'react';
+
+export const RingContext = createContext('ring')
 
 function App() {
   const [house, setHouse] =useState(5);
-  const ornaments = 'diamond ring'
+  const ornaments = 'Kada Mati ring'
   return (
-    <div className="App">
+    <RingContext.Provider value={ornaments}>
+            <div className="App">
       <button onClick={()=> setHouse(house + 1)}>Buy a new house</button>
       <GrandFather house={house} ornaments={ornaments}>
 
       </GrandFather>
       
     </div>
+    </RingContext.Provider>
   );
 }
 
